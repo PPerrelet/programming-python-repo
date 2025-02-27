e9773145df602f3734497870d8734112b6678e25
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-02-27 11:22:41 -0700

    Basic Operations exercise 3

---
+++ basic_operations/ex_3.py
@@ -0,0 +1,18 @@
+# What does the following code do? Why?
+
+print('5' + '10')
+print(type('5' + '10'))
+
+
+# This code prints "510" because 5 and 10 are enclosed with quotation marks.
+# Being enclosed in quotation marks deems the charater a string(str).
+# Instead of adding numberical values you are joining the characters together.
+# The proper terminology for this process is to "concatenate"
+# Concatenate is a process of combining two or more strings into a single larger string
+
+print("print('5' + '10')")
+print("The two digits in this equation are deemed strings because they are "
+      "enclosed in quotation marks.")
+print("So rather than adding the numerical values python adds the two strings together.")
+print("We call this concatenation and this results in 510")
+
