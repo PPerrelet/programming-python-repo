f9b9bd07161329a36593f0c19f256da31ec41f1c
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-02-27 09:43:48 -0700

    Basic Operations exercise 1

---
+++ basic_operations/ex_1.py
@@ -0,0 +1,10 @@
+# Concatenate two strings, one with your first name and one with your last, to create
+# a new string with your full name as its value. For example, if your name is John Doe,
+# you should combine 'John' and 'Doe' to get 'John Doe'.
+
+first_name = 'Patrick'
+last_name = 'Perrelet'
+
+print(f'{first_name} {last_name}')
+
+print('Patrick' + ' ' + 'Perrelet')
