a6c6478cfca23cc3562e0d79d2074c1df307c000
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-02-28 11:20:47 -0700

    Basic Operations exercise 5

---
+++ basic_operations/ex_5.py
@@ -0,0 +1,7 @@
+# Will an error occur if you try to access a list element with an index greater than or equal to the list's length? For example:
+
+#       0    1    2
+foo = ['a', 'b', 'c']
+print(foo[3])     # will this result in an error?
+
+# attempting to access an index greater than or equal to the lists length will result in an IndexError
