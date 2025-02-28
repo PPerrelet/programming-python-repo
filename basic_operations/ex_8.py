4aeb671fcfbe05ab710bb0718bbf3cef34f68af7
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-02-28 13:55:51 -0700

    Basic Operations exercise 8

---
+++ basic_operations/ex_8.py
@@ -0,0 +1,12 @@
+# To what value does the following expression evaluate?
+
+print('12' < '9')
+
+# This prints true because we are comparing 'strings' and the ASCII code value of 9 is greater than 12
+# Python compares the value of the of the strings by the first character in the string.
+# 1 < 9 
+# 49 < 57
+
+# You can convert these to integers if you wish to compare as such.
+
+print(int('12') < int('9'))
