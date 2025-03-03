1020f2279ec8da204e7c5b69d342c1578ec9b6bd
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-03-03 11:01:15 -0700

    Variables exercise 2

---
+++ variables/ex_2.py
@@ -0,0 +1,13 @@
+# Classify the following potential function names as idiomatic, non-idiomatic, or illegal. 
+# For the non-idiomatic and illegal names, explain your choice
+
+# index         idiomatic
+# CatName       non-idiomatic = this should be all lowercase seperated with an underscore.
+# lazy_dog      idiomatic
+# quick_Fox     non-idiomatic = should not contain capital letters.
+# 1stCharacter  illegal       = Can not start with a digit.
+# operand2      idiomatic
+# BIG_NUMBER    non-idiomatic = should be all lowercase letters.
+# π             non-idiomatic = not withing the ASCII table.
+
+print('Varibles and Functions follow the same naming conventions.')
