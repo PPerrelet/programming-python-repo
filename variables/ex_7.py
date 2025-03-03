467c2d297a1c86c6ed8bbf375ba77bd4b938295c
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-03-03 13:02:06 -0700

    Variables exercise 7

---
+++ variables/ex_7.py
@@ -0,0 +1,16 @@
+# What happens when you run the following code? Why?
+
+# Following code greets Victor 3 times 
+NAME = 'Victor'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+# Folling code greets Nina 3 times
+NAME = 'Nina'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+# "NAME" is a constant and should not change however python doesnt have a built in way to declare a constant.
+# Constants should be identified by SCREAMING_SNAKE_CASE.
