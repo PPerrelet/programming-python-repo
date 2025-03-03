d86822daf9af8a490bc2eee0832b15fd58ad2aff
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-03-03 11:59:31 -0700

    Variables exercise 5

---
+++ variables/greeter.py
@@ -0,0 +1,8 @@
+# Write a program named greeter.py that greets 'Victor' three times.
+# Your program should use a variable and not hard code the string value 'Victor' in each greeting.
+
+first_name = 'Victor'
+
+print(f'Hello, {first_name}.')
+print(f'Hola, {first_name}.')
+print(f'Bonjour, {first_name}.')
