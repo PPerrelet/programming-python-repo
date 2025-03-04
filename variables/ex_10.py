f0e3a268906b61409800f4287adaa12d884177f3
Patrick Perrelet <PatrickPerrelet@gmail.com>
2025-03-04 12:10:45 -0700

    Variables exercise 10

---
+++ variables/ex_10.py
@@ -0,0 +1,15 @@
+# Assume that obj already has a value of 42 when the code below starts running.
+#  Which of the subsequent statements reassign the variable?
+#  Which statements mutate the value of the object that obj references?
+#  Which statements do neither? If necessary, you can read the documentation.
+
+obj = 'ABcd'        # reassignmnet
+obj.upper()         # neither
+obj = obj.lower()   # reassignment
+print(len(obj))     # neither
+obj = list(obj)     # reassignmnet
+obj.pop()           # mutation
+obj[2] = 'X'        # mutation
+obj.sort()          # mutation
+set(obj)            # neither
+obj = tuple(obj)    # reassignmnet
